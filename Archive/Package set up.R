74f34a3d28938ee1525f9258008eb272646e3448
JackAuty <jack.auty@utas.edu.au>
2025-10-21 14:13:24 +1100

    Initial commit

--- Archive/Package set up.R
+++ Archive/Package set up.R
@@ -1,8 +1,16 @@
-# Turn this folder into a git repo, add everything, make first commit
-library(gert)
-git_init(".")
-git_add(".")
-git_commit("Initial OmicAdjust package commit")
+# make sure usethis is available
+install.packages("usethis")  # skip if already installed
+library(usethis)
 
-# Create the GitHub repo and push (uses your saved GitHub token)
-usethis::use_github(private = FALSE)
+# ensure we're operating in this project (no new windows)
+proj_set(".", force = TRUE)
+
+# (one-time) set your Git identity, globally
+use_git_config(user.name = "JackAuty", user.email = "jack.auty@utas.edu.au")
+
+# initialise Git here and make the first commit
+use_git()  # RStudio will likely ask to restart once; click "Yes"
+
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+
+use_github(private = FALSE)
