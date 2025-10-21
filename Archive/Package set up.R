2a4b67566775841b8cf3bb366ea7a2c89523d9c7
JackAuty <jack.auty@utas.edu.au>
2025-10-21 14:22:32 +1100

    Initial commit

--- Archive/Package set up.R
+++ Archive/Package set up.R
@@ -8,3 +8,13 @@ use_git_config(user.name = "JackAuty", user.email = "jack.auty@utas.edu.au")
 # Initialise Git for this folder and make the first commit
 # RStudio will ask to restart once; click "Yes".
 use_git()
+
+
+proj_set(".", force = TRUE)
+
+# If you haven't saved a GitHub token on this machine yet, do these once:
+# create_github_token()
+# gitcreds::gitcreds_set()  # paste the token when prompted
+
+# Create `JackAuty/OmicAdjust` on GitHub and push the current branch
+use_github(private = FALSE)
