3b7bb8d458cb099960d147448ba17b01da06dfe1
JackAuty <jack.auty@utas.edu.au>
2025-10-21 14:14:52 +1100

    Initial commit

--- Archive/Package set up.R
+++ Archive/Package set up.R
@@ -1,16 +1,10 @@
-# make sure usethis is available
-install.packages("usethis")  # skip if already installed
+# Use usethis in THIS project (no new windows)
 library(usethis)
-
-# ensure we're operating in this project (no new windows)
 proj_set(".", force = TRUE)
 
-# (one-time) set your Git identity, globally
+# (One-time global identity; safe to run again)
 use_git_config(user.name = "JackAuty", user.email = "jack.auty@utas.edu.au")
 
-# initialise Git here and make the first commit
-use_git()  # RStudio will likely ask to restart once; click "Yes"
-
-usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
-
-use_github(private = FALSE)
+# Initialise Git for this folder and make the first commit
+# RStudio will ask to restart once; click "Yes".
+use_git()
